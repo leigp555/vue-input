80a80b9cae295db401ac70363b1830847d2a7b44
leigp <907090585@qq.com>
2021-10-18 15:18:34 +0800

    模拟input

---
+++ src/components/model.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <input :value="value" @input="onInput" />
+  </div>
+</template>
+<script>
+export default {
+  name:"MyInput",
+  props:["value"],
+  methods:{
+    onInput(e){
+      const data =e.target.value
+      this.$emit("input",data)
+    }
+  }
+}
+</script>
+<style scoped>
+
+</style>
---
+++ src/main.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    {{user.userName}}
+    <hr>
+    {{user.userPassword}}
+    <myInput :value="user.userName" @input="user.userName=$event" />
+    <myInput v-model="user.userPassword" />
+  </div>
+
+</template>
+<script>
+import myInput from "./components/model"
+
+export default {
+  components: {myInput},
+  data(){
+    return {
+      user:{
+        userName:"李白",
+        userPassword:"123"
+      }
+    }
+  }
+}
+</script>
+<style scoped>
+
+</style>
